ea5bec7e8f6b9764afe0065dfa9c79ffc94b454c
iguinho-ux <igormachadosr@gmail.com>
2025-04-16 09:39:03 -0300

    atv01

---
+++ listaif/Program.cs
@@ -0,0 +1,10 @@
+﻿
+string numero = Console.ReadLine();
+double numeroconvertido = Convert.ToDouble(numero);
+bool positivo = numeroconvertido >= 0;
+if (positivo)
+{
+
+    Console.WriteLine("positivo");
+
+}
